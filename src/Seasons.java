0bc3e2b818dc1678a63c87c7a0b0f77f30f5a04d
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-21 10:44:52 +0300

    third task numbers

--- src/Seasons.java
+++ src/Seasons.java
@@ -43,7 +43,5 @@ public class Seasons {
         } else {
             System.out.println(other);
         }
-
-
     }
 }
---
+++ src/Values.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author Belitski Marat
+ * @date 21.12.2023
+ * @project TMS_AnOnl_17_Lesson_3
+ */
+public class Values {
+    public static void main(String[] args) {
+
+        //Задача № 3
+        Scanner scanner = new Scanner(System.in);
+        String input = "Введите число: ";
+
+        System.out.print(input);
+        while (scanner.hasNext()) {
+
+            int choose = scanner.nextInt();
+
+            int result = choose % 2;
+            if (result == 0) {
+                System.out.println("Это четное число");
+                System.out.print(input);
+            } else {
+                System.out.println("Это нечетное число");
+                System.out.print(input);
+            }
+        }
+    }
+}
