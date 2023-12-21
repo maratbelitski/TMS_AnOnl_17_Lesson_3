6503b73d5086b976f0f90a9460683eec9d7bb08f
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-21 11:30:57 +0300

    sixth task cycles

---
+++ src/Cycles.java
@@ -0,0 +1,16 @@
+/**
+ * @author Belitski Marat
+ * @date 21.12.2023
+ * @project TMS_AnOnl_17_Lesson_3
+ */
+public class Cycles {
+    public static void main(String[] args) {
+
+        //Задача № 1
+        for (int i = 1; i < 100; i++) {
+            if (i%2 !=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
