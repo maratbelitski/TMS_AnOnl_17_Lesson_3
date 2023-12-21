6d6482c5ca006e86511e15464c244be3a1d67d4d
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-21 13:43:25 +0300

    thirteenth task bank

--- src/Advance.java
+++ src/Advance.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Belitski Marat
  * @date 21.12.2023
@@ -5,13 +7,28 @@
  */
 public class Advance {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         //Задача № 3
         for (int i = 2; i < 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                System.out.print(j*i+" ");
+                System.out.print(j * i + " ");
             }
             System.out.println();
         }
+        System.out.println();
+
+
+        //Задача № 2
+        System.out.print("Введите сумму вклада: ");
+        float deposit = scanner.nextFloat();
+        System.out.print("Введите срок вклада в месяцах: ");
+        int term = scanner.nextInt();
+        float percent = 7.0F;
+
+        for (int i = 1; i <= term; i++) {
+            deposit = deposit + (deposit / 100 * percent);
+        }
+        System.out.printf("Сумма средств по окончанию договора: %.2f рублей", deposit);
     }
 }
