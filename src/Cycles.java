7c9835b743d0390898b9b60ea2dc497890c7ff40
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-21 12:10:17 +0300

    ninth task sum while

--- src/Cycles.java
+++ src/Cycles.java
@@ -11,24 +11,38 @@ public class Cycles {
 
         //Задача № 1
         for (int i = 1; i < 100; i++) {
-            if (i%2 !=0){
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
 
         //Задача № 2
         for (int i = 5; i > 0; i--) {
-                System.out.println(i);
+            System.out.println(i);
         }
 
 
         //Задача № 3
         System.out.print("Введите число до которого нужно суммировать: ");
         int input = scanner.nextInt();
-        int temp=0;
+        int temp = 0;
         for (int i = 1; i < input; i++) {
             temp += i;
         }
-        System.out.printf("Сумма чисел от 1 до %S (не включительно) равна %s",input,temp);
+        System.out.printf("Сумма чисел от 1 до %S (не включительно) равна %s", input, temp);
+
+
+        //Задача № 4
+        boolean exit = false;
+        int number = 0;
+
+        while (!exit) {
+            number += 7;
+            System.out.println(number);
+
+            if (number == 98) {
+                exit = true;
+            }
+        }
     }
 }
