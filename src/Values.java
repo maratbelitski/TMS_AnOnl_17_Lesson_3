b8d6b3285bc1b99ad553814fde05a1fae3e03172
maratbelitski <marat.belitckiy2010@yandex.com>
2023-12-21 11:19:57 +0300

    fifth task rainbow

--- src/Values.java
+++ src/Values.java
@@ -39,5 +39,21 @@ public class Values {
         }else {
             System.out.println("Нормально");
         }
+
+
+        //Задача № 5
+        System.out.print("Введите порядковый номер цвета: ");
+        int color = scanner.nextInt();
+
+        switch (color){
+            case 1 -> System.out.println("Красный");
+            case 2 -> System.out.println("Оранжевый");
+            case 3 -> System.out.println("Желтый");
+            case 4 -> System.out.println("Зеленый");
+            case 5 -> System.out.println("Голубой");
+            case 6 -> System.out.println("Синий");
+            case 7 -> System.out.println("Фиолетовый");
+            default -> System.out.println("Такого цвета не существует !");
+        }
     }
 }
